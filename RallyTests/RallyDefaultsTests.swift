2404cd4e420ef70e90dae7563fb2c49c72c828e3
marcelo zapata <marcelozapata00@gmail.com>
2026-06-21 13:36:07 -0400

    [CX] Guard quiet sound defaults with tests

---
+++ RallyTests/RallyDefaultsTests.swift
@@ -0,0 +1,54 @@
+import XCTest
+@testable import Rally
+
+final class RallyDefaultsTests: XCTestCase {
+
+    private var defaults: UserDefaults!
+    private var suiteName: String!
+
+    override func setUp() {
+        super.setUp()
+        suiteName = "RallyDefaultsTests.\(UUID().uuidString)"
+        defaults = UserDefaults(suiteName: suiteName)!
+        clearSoundKeys()
+    }
+
+    override func tearDown() {
+        clearSoundKeys()
+        defaults.removePersistentDomain(forName: suiteName)
+        defaults = nil
+        suiteName = nil
+        super.tearDown()
+    }
+
+    func testSoundDefaultsToOffForFreshInstall() {
+        XCTAssertFalse(RallyDefaults.resolvedSoundEnabled(defaults))
+    }
+
+    func testQuietDefaultMigrationForcesSoundOffOnce() {
+        defaults.set(true, forKey: UserDefaultsKeys.soundEnabled)
+
+        RallyDefaults.applyQuietSoundDefaultIfNeeded(defaults)
+
+        XCTAssertTrue(defaults.bool(forKey: UserDefaultsKeys.soundQuietDefaultApplied))
+        XCTAssertTrue(defaults.bool(forKey: UserDefaultsKeys.soundPreferenceExplicitlySet))
+        XCTAssertFalse(defaults.bool(forKey: UserDefaultsKeys.soundEnabled))
+        XCTAssertFalse(RallyDefaults.resolvedSoundEnabled(defaults))
+    }
+
+    func testPlayerCanOptBackIntoSoundAfterMigration() {
+        RallyDefaults.applyQuietSoundDefaultIfNeeded(defaults)
+        defaults.set(true, forKey: UserDefaultsKeys.soundEnabled)
+        defaults.set(true, forKey: UserDefaultsKeys.soundPreferenceExplicitlySet)
+
+        RallyDefaults.applyQuietSoundDefaultIfNeeded(defaults)
+
+        XCTAssertTrue(RallyDefaults.resolvedSoundEnabled(defaults))
+    }
+
+    private func clearSoundKeys() {
+        defaults.removeObject(forKey: UserDefaultsKeys.soundEnabled)
+        defaults.removeObject(forKey: UserDefaultsKeys.soundPreferenceExplicitlySet)
+        defaults.removeObject(forKey: UserDefaultsKeys.soundQuietDefaultApplied)
+    }
+}
